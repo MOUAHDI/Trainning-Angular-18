582ac94b5def32101ba889b08c3eb8b9b50f421e
RSamaium <sam@webcreative5.net>
2024-06-11 09:33:57 +0200

    content child

--- src/app/features/users/user-card/user-card.component.ts
+++ src/app/features/users/user-card/user-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { AfterContentInit, AfterViewInit, Component, ContentChild, ElementRef, Input, OnInit } from "@angular/core";
 import { User } from "../../../../core/interfaces/user.interface";
 import { LangPipe } from "../../../shared/pipes/lang.pipe";
 
@@ -6,16 +6,24 @@ import { LangPipe } from "../../../shared/pipes/lang.pipe";
     selector: 'app-user-card',
     template: `
         <article>
+            <ng-content select="h1"></ng-content>
             <header>{{ user.name }}</header>
              {{ user.email }}
+             <ng-content select="h2"></ng-content>
             <footer>
                 <button>{{ 'REMOVE' | lang:'fr' }}</button>
             </footer>
+            
         </article>
     `,
     standalone: true,
     imports: [LangPipe]
 })
-export class UserCardComponent {
+export class UserCardComponent implements AfterContentInit {
     @Input() user: User = {} as User
+    @ContentChild('title') myTitle!: ElementRef
+
+    ngAfterContentInit(): void {
+        console.log(this.myTitle)
+    }
 }
