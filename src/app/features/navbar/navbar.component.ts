0d67c18ddc1ea8683606d4d57d3e5e6e6b93c40d
RSamaium <sam@webcreative5.net>
2024-06-11 10:24:58 +0200

    router

--- src/app/features/navbar/navbar.component.ts
+++ src/app/features/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component } from "@angular/core";
 import { SearchComponent } from "./search/search.component";
 import { CurrencyPipe, UpperCasePipe } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-navbar',
     template: `
         <h1>{{ title | uppercase }} - {{ price | currency:'EUR':'code' }}</h1>
         <app-search [userName]="name" (eventSearch)="listenSearch($event)" />
+        <button routerLink="/login">Se connecter</button>
     `,
     standalone: true,
-    imports: [SearchComponent, UpperCasePipe, CurrencyPipe]
+    imports: [SearchComponent, UpperCasePipe, CurrencyPipe, RouterLink]
 })
 export class NavbarComponent {
     title = 'Mon App'
